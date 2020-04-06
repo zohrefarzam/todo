ac60d326a0347a891fdf4878315532ac5fd1b7e1
zohre <zohrefarzam@gmail.com>
2020-04-06 10:50:54 -0700

    Updates

---
+++ precache-manifest.d7000432f99704b3d03e38754d531e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdaefccbe4603f1306db4e9317e1238",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4c64d659fb46b5de3034",
+    "url": "/todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66efc92aed70ed09a030",
+    "url": "/todo/static/js/2.514cfc12.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo/static/js/2.514cfc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c64d659fb46b5de3034",
+    "url": "/todo/static/js/main.21f52e73.chunk.js"
+  },
+  {
+    "revision": "7ebb65d26b1e638348aa",
+    "url": "/todo/static/js/runtime-main.7bba6494.js"
+  }
+]);
